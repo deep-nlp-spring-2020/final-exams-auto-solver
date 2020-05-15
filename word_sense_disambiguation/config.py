a1a24a1861f998270f8dd145c992357505f66838
zhav1k <42671973+zhav1k@users.noreply.github.com>
2020-05-15 23:33:45 +0300

    Add files via upload

---
+++ word_sense_disambiguation/config.py
@@ -0,0 +1,7 @@
+MYPATH_TRAIN = '../public_set/train/'
+MYPATH_TEST = '../public_set/test/'
+MYPATH_VAL = '../public_set/val/'
+
+LAYERS_TO_RETRIEVE = [-1, -2, -3, -4]
+
+METHODS_TO_EMBED = ['concat_last_n_layers_cls', 'concat_last_n_layers_over_all']
